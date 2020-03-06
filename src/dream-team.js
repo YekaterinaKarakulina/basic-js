88d47da45b94b36c4f7eede7b464d3cf1d8a1c30
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-03-06 17:24:27 +0600

    feat: dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,21 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  var dreamTeam = [];
+  let regex = /^([a-z 0_A-Z]+)$/;
+  if(members==null) {
+    return false;
+  } else if (typeof(members)=='object') {
+    for(var i=0; i<members.length; i++) {
+      if(typeof members[i]==='string')       {
+        if(regex.test(members[i]))         {
+          dreamTeam.push(members[i].trim().charAt(0));
+        }
+      }
+    }
+    dreamTeam = Array.from(dreamTeam).sort((a, b) => {
+      return a.localeCompare(b, 'en', { sensitivity: 'base' });
+    });
+    return dreamTeam.join('').toUpperCase();
+  } else {
+    return false;
+  }
 };
