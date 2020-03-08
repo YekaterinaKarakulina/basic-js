03674c6877a0602372162e9548e78a131d6599e7
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-03-08 16:21:44 +0600

    feat: what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,23 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  //TODO add some regex 
+  var month;
+  if(typeof date == 'object') {
+    month = date.getMonth();
+    if(month >=0 && month <2) {
+      return 'winter';
+    } else if(month==11) {
+      return 'winter';
+    } else if(month >=2 && month <5) {
+      return 'spring';
+    } else if(month >= 5 && month<8) {
+      return 'summer';
+    } else if(month>= 8 && month <11) {
+      return 'fall';
+    } else {
+    throw 'Error';
+    }
+  }
+  if (date == null) {
+  return 'Unable to determine the time of year!';
+  }
 };
