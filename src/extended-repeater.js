2265daa6d91f841e161f75bf07c34749730f09ac
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-03-15 18:08:54 +0600

    feat: extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,49 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    var repeatTimes = 1;
+    var separator = '';
+    var addition = '';
+    var additionRepeatTimes = 0;
+    var additionSeparator = '';
+    var newStr = '';
+    var finalAddition = '';
+
+    if(typeof(options.repeatTimes) == 'undefined') {
+        repeatTimes = 1;
+    } else {
+        repeatTimes = options.repeatTimes;
+    }
+
+    if(typeof(options.separator) == 'undefined') {
+        separator = '+';
+    } else {
+        separator = options.separator;
+    }
+    
+    if(typeof(options.addition) == 'undefined') {
+        addition = '';
+    } else {
+        addition = options.addition;
+    }
+    
+    if(typeof(options.additionRepeatTimes) == 'undefined') {
+        additionRepeatTimes = 1;
+    } else {
+        additionRepeatTimes = options.additionRepeatTimes;
+    }
+
+    additionSeparator = options.additionSeparator;
+    
+    for(var j=0; j<additionRepeatTimes-1; j++)
+    {
+        finalAddition = finalAddition + (addition + additionSeparator);
+    }
+
+    finalAddition = finalAddition + addition;
+    
+    for(var i=0; i<repeatTimes-1; i++)
+    {
+        newStr = newStr + (str + (finalAddition) + separator);
+    }
+    newStr = newStr + str + finalAddition;
+    return newStr;
 };
-  
